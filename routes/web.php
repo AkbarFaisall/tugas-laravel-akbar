a277bc682abb4277859365c0538064c71724f065
AkbarFaisall <muhammadakbarfaisal1@gmail.com>
2026-04-18 20:27:54 +0800

    tugas laravel

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-/**
- * TUGAS PERTEMUAN 4
- * Menghubungkan URL dengan ProductController
- */
-
-// Menampilkan semua data (READ) 
-Route::get('/products', [ProductController::class, 'index']);
-
-// Menambah data baru (CREATE) 
-Route::post('/products', [ProductController::class, 'store']);
-
-// Mengubah data (UPDATE) 
-Route::put('/products/{id}', [ProductController::class, 'update']);
-
-// Menghapus data (DELETE) 
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
